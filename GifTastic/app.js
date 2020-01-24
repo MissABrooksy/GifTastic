192fd20c9948810ccb9e3139fc7049b6191f45a1
Andrea <andreakwilliams1@gmail.com>
2020-01-24 18:00:35 -0500

    api

---
+++ GifTastic/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+ 
+  var topics = ["red", "yellow", "blue", "green", "orange", "white", "purple", "pink", "teal"];
+
+  // function to make buttons and add to page
+  function addButtons(topics, newGifs) {
+    $(newGifs).empty();
+
+    for (var i = 0; i < topics.length; i++) {
+      var color = $("<button>");
+      $(newGifs).append(color);
+    }
+  }
+});
+
+  
+
+function getGifs(){
+
+  var input = $("gifInput").val()
+
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+input+"&api_key=UMpcHdf7DsNyxd6QLhzS03psCOF65go0&limit=10");
+
+console.log(input);
+xhr.done(function(response) { 
+  console.log("success got data", response); 
+   
+  var gifs = response.data
+  for (i in gifs)
+  {
+
+      $('.newGifs').append('<img src="'+gifs[i].images.original.url+'" />');
+      $('.newGifs').append('<img src="'+gifs[i].ratings+'" />')
+  }
+
+    
+
+});
+}
+getGifs();
